bff3bf876dc161177fcf19d3f1733504cc8dbc97
Mario Konrad <mario.konrad@gmx.net>
2012-09-29 00:13:44 +0200

    lua filter prepared

--- src/navcom/filter/filter_lua.c
+++ src/navcom/filter/filter_lua.c
@@ -3,6 +3,40 @@
 #include <string.h>
 #include <syslog.h>
 #include <lua/lua.h>
+#include <lua/lualib.h>
+#include <lua/lauxlib.h>
+
+static int configure(
+		struct filter_context_t * ctx,
+		const struct property_list_t * properties)
+{
+	lua_State * lua = NULL;
+
+	UNUSED_ARG(properties);
+
+	/* TODO: property: script */
+
+	lua = luaL_newstate();
+	luaopen_base(lua);
+	luaopen_table(lua);
+	luaopen_string(lua);
+	luaopen_math(lua);
+	ctx->data = lua;
+
+	return FILTER_SUCCESS;
+}
+
+int free_context(
+		struct filter_context_t * ctx)
+{
+	if (ctx && ctx->data) {
+		lua_State * lua = (lua_State *)ctx->data;
+		lua_close(lua);
+		ctx->data = NULL;
+	}
+
+	return FILTER_SUCCESS;
+}
 
 static int filter(
 		struct message_t * out,
@@ -10,20 +44,24 @@ static int filter(
 		struct filter_context_t * ctx,
 		const struct property_list_t * properties)
 {
+	lua_State * lua = NULL;
+
 	UNUSED_ARG(out);
 	UNUSED_ARG(in);
-	UNUSED_ARG(ctx);
 	UNUSED_ARG(properties);
+
+	if (!ctx || !ctx->data) return FILTER_FAILURE;
+	lua = (lua_State *)ctx->data;
 
 	/* TODO */
 
-	return FILTER_FAILURE;
+	return FILTER_DISCARD;
 }
 
-const struct filter_desc_t filter_nmea = {
+const struct filter_desc_t filter_lua = {
 	.name = "filter_lua",
-	.configure = NULL,
-	.free_ctx = NULL,
+	.configure = configure,
+	.free_ctx = free_context,
 	.func = filter
 };
 
