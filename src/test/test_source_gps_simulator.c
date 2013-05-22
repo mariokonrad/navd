881cf4e800372f69cca02ba7859c98eaace9d67c
Mario Konrad <mario.konrad@gmx.net>
2013-05-22 11:23:38 +0200

    Some unittests for source src_lua and gps_simulator.

--- src/test/test_source_gps_simulator.c
+++ src/test/test_source_gps_simulator.c
@@ -3,12 +3,91 @@
 #include <navcom/source/gps_simulator.h>
 #include <common/macros.h>
 #include <string.h>
+#include <stdlib.h>
 
 static const struct proc_desc_t * proc = &gps_simulator;
 
-static void test_(void)
+static void test_existance(void)
 {
-	CU_FAIL();
+	CU_ASSERT_PTR_NOT_NULL(proc);
+	CU_ASSERT_PTR_NOT_NULL(proc->init);
+	CU_ASSERT_PTR_NOT_NULL(proc->func);
+	CU_ASSERT_PTR_NULL(proc->exit);
+}
+
+/**
+ * @todo Check resuls of after init
+ */
+static void test_init(void)
+{
+	struct property_list_t properties;
+
+	proplist_init(&properties);
+
+	CU_ASSERT_EQUAL(proc->init(NULL, NULL), EXIT_FAILURE);
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* period */
+	proplist_set(&properties, "period", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "period", "1000");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* sog */
+	proplist_set(&properties, "sog", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "sog", "123");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* heading */
+	proplist_set(&properties, "heading", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "heading", "123");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* mag */
+	proplist_set(&properties, "mag", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "mag", "3");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* date */
+	proplist_set(&properties, "date", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "date", "2013-05-22");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* time */
+	proplist_set(&properties, "time", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "time", "12-34");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* lat */
+	proplist_set(&properties, "lat", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "lat", "12-34,5N");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* lon */
+	proplist_set(&properties, "lon", "xyz");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_FAILURE);
+
+	proplist_set(&properties, "lon", "123-45,6N");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	/* simulated */
+	proplist_set(&properties, "__not_simulated__", "");
+	CU_ASSERT_EQUAL(proc->init(NULL, &properties), EXIT_SUCCESS);
+
+	proplist_free(&properties);
 }
 
 void register_suite_source_gps_simulator(void)
@@ -16,6 +95,7 @@ void register_suite_source_gps_simulator(void)
 	CU_Suite * suite;
 	suite = CU_add_suite("source/gps_simulator", NULL, NULL);
 
-	CU_add_test(suite, "?", test_);
+	CU_add_test(suite, "existance", test_existance);
+	CU_add_test(suite, "init", test_init);
 }
 
--- src/test/test_source_src_lua.c
+++ src/test/test_source_src_lua.c
@@ -3,19 +3,202 @@
 #include <navcom/source/src_lua.h>
 #include <common/macros.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <lua/lua.h>
 
 static const struct proc_desc_t * proc = &src_lua;
+static char tmpfilename[PATH_MAX];
+static int fd = -1;
 
-static void test_(void)
+static int setup(void)
 {
-	CU_FAIL();
+	strncpy(tmpfilename, "/tmp/test_filter_luaXXXXXXXX", sizeof(tmpfilename));
+	fd = mkstemp(tmpfilename);
+	return fd < 0;
+}
+
+static int cleanup(void)
+{
+	close(fd);
+	unlink(tmpfilename);
+	return 0;
+}
+
+static void prepare_script(const char * code)
+{
+	int rc;
+
+	rc = ftruncate(fd, 0);
+	CU_ASSERT_EQUAL(rc, 0);
+
+	rc = write(fd, code, strlen(code));
+	CU_ASSERT_EQUAL(rc, (int)strlen(code));
+
+	rc = lseek(fd, SEEK_SET, 0);
+	CU_ASSERT_EQUAL(rc, 0);
+}
+
+static void test_existance(void)
+{
+	CU_ASSERT_PTR_NOT_NULL(proc);
+	CU_ASSERT_PTR_NOT_NULL(proc->init);
+	CU_ASSERT_PTR_NOT_NULL(proc->func);
+	CU_ASSERT_PTR_NOT_NULL(proc->exit);
+}
+
+static void test_exit(void)
+{
+	CU_ASSERT_EQUAL(proc->exit(NULL), EXIT_FAILURE);
+}
+
+/**
+ * @todo Check for values after 'init'
+ */
+static void test_init(void)
+{
+	struct property_list_t properties;
+	struct proc_config_t config;
+
+	proc_config_init(&config);
+	proplist_init(&properties);
+
+	CU_ASSERT_EQUAL(proc->init(NULL, NULL), EXIT_FAILURE);
+
+	proplist_set(&properties, "period", "1000");
+	CU_ASSERT_EQUAL(proc->init(&config, &properties), EXIT_SUCCESS);
+	CU_ASSERT_EQUAL(proc->exit(&config), EXIT_SUCCESS);
+
+	proplist_set(&properties, "period", "xyz");
+	CU_ASSERT_EQUAL(proc->init(&config, &properties), EXIT_FAILURE);
+	CU_ASSERT_EQUAL(proc->exit(&config), EXIT_SUCCESS);
+
+	proplist_free(&properties);
+}
+
+static void test_init_noscript(void)
+{
+	struct proc_config_t config;
+	struct property_list_t properties;
+	int rc;
+
+	proc_config_init(&config);
+
+	proplist_init(&properties);
+	proplist_set(&properties, "script", "/dev/null");
+	proplist_set(&properties, "period", "1000");
+
+	rc = proc->init(&config, &properties);
+	CU_ASSERT_EQUAL(rc, EXIT_FAILURE);
+
+	rc = proc->exit(&config);
+	CU_ASSERT_EQUAL(rc, EXIT_SUCCESS);
+
+	proplist_free(&properties);
+}
+
+static void test_init_emptyscript(void)
+{
+	struct proc_config_t config;
+	struct property_list_t properties;
+	int rc;
+
+	const char SCRIPT[] = "\n";
+
+	proc_config_init(&config);
+
+	proplist_init(&properties);
+	proplist_set(&properties, "script", tmpfilename);
+	proplist_set(&properties, "period", "1000");
+
+	prepare_script(SCRIPT);
+
+	rc = proc->init(&config, &properties);
+	CU_ASSERT_EQUAL(rc, EXIT_SUCCESS);
+
+	rc = proc->exit(&config);
+	CU_ASSERT_EQUAL(rc, EXIT_SUCCESS);
+
+	proplist_free(&properties);
+}
+
+static void test_init_invalidscript(void)
+{
+	struct proc_config_t config;
+	struct property_list_t properties;
+	int rc;
+
+	const char SCRIPT[] =
+		"fucntion def()\n"
+		"	local a = 1\n"
+		"end\n";
+
+	proc_config_init(&config);
+
+	proplist_init(&properties);
+	proplist_set(&properties, "script", tmpfilename);
+	proplist_set(&properties, "period", "1000");
+
+	prepare_script(SCRIPT);
+
+	rc = proc->init(&config, &properties);
+	CU_ASSERT_EQUAL(rc, EXIT_FAILURE);
+
+	rc = proc->exit(&config);
+	CU_ASSERT_EQUAL(rc, EXIT_SUCCESS);
+
+	proplist_free(&properties);
+}
+
+static void test_init_scripterror(void)
+{
+	struct proc_config_t config;
+	struct property_list_t properties;
+	lua_State * lua;
+	int rc;
+
+	const char SCRIPT[] =
+		"function abc(t)\n"
+		"	return t.def + t.def\n"
+		"end\n"
+		"function def()\n"
+		"	abc({})\n"
+		"end\n";
+
+	proc_config_init(&config);
+
+	proplist_init(&properties);
+	proplist_set(&properties, "script", tmpfilename);
+	proplist_set(&properties, "period", "1000");
+
+	prepare_script(SCRIPT);
+
+	rc = proc->init(&config, &properties);
+	CU_ASSERT_EQUAL_FATAL(rc, EXIT_SUCCESS);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(config.data);
+
+	lua = (lua_State *)config.data;
+	lua_getglobal(lua, "def");
+	lua_pcall(lua, 0, 0, 0);
+
+	rc = proc->exit(&config);
+	CU_ASSERT_EQUAL(rc, EXIT_SUCCESS);
+
+	proplist_free(&properties);
 }
 
 void register_suite_source_src_lua(void)
 {
 	CU_Suite * suite;
-	suite = CU_add_suite("source/src_lua", NULL, NULL);
+	suite = CU_add_suite("source/src_lua", setup, cleanup);
 
-	CU_add_test(suite, "?", test_);
+	CU_add_test(suite, "existance", test_existance);
+	CU_add_test(suite, "exit", test_exit);
+	CU_add_test(suite, "init", test_init);
+	CU_add_test(suite, "init: no script", test_init_noscript);
+	CU_add_test(suite, "init: empty script", test_init_emptyscript);
+	CU_add_test(suite, "init: invalid script", test_init_invalidscript);
+	CU_add_test(suite, "init: script error", test_init_scripterror);
 }
 
